3f5176af6b2ddd01e1c53b5d86c54bfb654f6051
Dan Stoppelman <dstop75@gmail.com>
2015-04-05 16:28:25 -0400

    authenticate on products#create, #update, #destroy

--- backend/app/controllers/products_controller.rb
+++ backend/app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_filter :authenticate, only: [:create, :update, :destroy]
   before_action :set_product, only: [:show, :update, :destroy]
 
   # GET /products
