31a86d67c6996196e18413ca59c6425c3dbe291a
Dan Stoppelman <dstop75@gmail.com>
2015-04-05 16:11:45 -0400

    config auth and serialization

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  include ActionController::Serialization
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  def default_serializer_options
+    {root: false}
+  end
+
+  protected
+
+    def authenticate
+      authenticate_or_request_with_http_token do |token, options|
+        @current_user = User.find_by(token: token)
+      end
+    end
 end
