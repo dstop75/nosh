4b94f39a33aea9124cd161c912931de058609441
Dan Stoppelman <dstop75@gmail.com>
2015-04-05 16:55:56 -0400

    add validations and #generate_token

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  has_secure_password
+
   has_one :cart
   has_many :orders
+
+  def generate_token
+    return if token.present?
+    begin
+      self.token = SecureRandom.uuid.gsub(/\-/, '')
+    end while self.class.exists?(token: token)
+  end
 end
