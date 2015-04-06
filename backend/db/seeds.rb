8fc846f812e0c83befd83ceac8bc3853119755c5
Dan Stoppelman <dstop75@gmail.com>
2015-04-06 18:50:19 -0400

    add 3 fake users to seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([
+  {
+    email: "katelyn@heelheathcote.com",
+     first_name: "Creola",
+     last_name: "Cummings",
+     password: "autem",
+     password_digest: "$2a$10$5e.hnel2jFHrgm3f1bjjNuhJ46Svf2EpzHMkQsUCslvpFrxD56tk2",
+     token: "8tmgt4enhr2bcaoahcpuc7sd18xt1cdc",
+     admin: true
+    },
+    {
+      email: "margarette_raynor@johnsklein.info",
+      first_name: "Delaney",
+      last_name: "Ernser",
+      password: "error",
+      password_digest: "$2a$10$Gu3CLPH1GnGhkAQL5m8BBu3RzfRCJvueosaEnIt4n3nDfDFM4XoKS",
+      token: "2xtu187ymywgcoye3m772sfotkwef4dk",
+      admin: false
+      },
+      {
+        email: "michale.kshlerin@sawaynwindler.name",
+        first_name: "Ulices",
+        last_name: "Walsh",
+        password: "asperiores",
+        password_digest: "$2a$10$kBJ62/zxa8HWwsV/.yAgFexBk8JkqLRzbW5DpydxZ4irm9TDwvsqu",
+        token: "hqjb423sqc09n6f5pd47ah7f044b70ou",
+        admin: false
+        }])
