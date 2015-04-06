5ab796ca265aca48d1675beb6d69a6766c240350
Dan Stoppelman <dstop75@gmail.com>
2015-04-06 18:19:26 -0400

    make #generate_token private

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,17 +3,19 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-  has_secure_password
-
   has_one :cart
   has_many :orders
 
+  has_secure_password
+
   before_create :generate_token
 
-  def generate_token
-    return if token.present?
-    begin
-      self.token = SecureRandom.uuid.gsub(/\-/, '')
-    end while self.class.exists?(token: token)
-  end
+  private
+
+    def generate_token
+      return if token.present?
+      begin
+        self.token = SecureRandom.uuid.gsub(/\-/, '')
+      end while self.class.exists?(token: token)
+    end
 end
