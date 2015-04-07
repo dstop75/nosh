d52103ce85d081a886306bacda53e59828bd88e3
Dan Stoppelman <dstop75@gmail.com>
2015-04-07 10:16:25 -0400

    add Login, Main, and Navbar directives, basic login and show products now functional

---
+++ frontend/app/scripts/directives/NavbarDirective.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('MainController').directive('ncNavbar', [function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/navbar.html',
+        controller: 'NavbarController',
+        controllerAs: 'navbarController',
+        bindToController: true,
+        scope: {},
+        link: function($scope, element, attrs) {
+            //
+        }
+    };
+}]);
