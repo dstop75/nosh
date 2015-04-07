ba079b332bcf074e2899c73cad3b14869b149ef8
Dan Stoppelman <dstop75@gmail.com>
2015-04-07 10:12:34 -0400

    add LoginController

---
+++ frontend/app/scripts/controllers/LoginController.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('MainController').controller('LoginController', loginController);
+
+loginController.$inject = ['AuthFactory', '$location'];
+
+function loginController(AuthFactory, $location) {
+    var vm = this;
+
+    vm.login = function(credentials) {
+        AuthFactory.login(credentials).then(
+            function(response) {
+                vm.credentials = {};
+                $location.path('/');
+            });
+    };
+}
