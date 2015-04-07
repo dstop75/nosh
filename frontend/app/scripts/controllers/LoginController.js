f40b94fb6bea4c5e97ad7969388c5043c3e9142b
Dan Stoppelman <dstop75@gmail.com>
2015-04-07 17:34:44 -0400

    whitespace

--- frontend/app/scripts/controllers/LoginController.js
+++ frontend/app/scripts/controllers/LoginController.js
@@ -8,10 +8,9 @@ function loginController(AuthFactory, $location) {
     var vm = this;
 
     vm.login = function(credentials) {
-        AuthFactory.login(credentials).then(
-            function(response) {
-                vm.credentials = {};
-                $location.path('/');
-            });
+        AuthFactory.login(credentials).then(function(response) {
+            vm.credentials = {};
+            $location.path('/');
+        });
     };
 }
