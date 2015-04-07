7aadf6a041029fef2c6c0182379ee064d37a0701
Dan Stoppelman <dstop75@gmail.com>
2015-04-07 10:08:57 -0400

    setup app.js dependencies, run function, etc.

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -9,22 +9,31 @@
  * Main module of the application.
  */
 angular
-  .module('frontendApp', [
-    'ngAnimate',
-    'ngRoute',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('NoshApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'MainController',
+        'MainDirective'
+        ])
+    .run(function($rootScope, $http, $window, $location, AuthFactory, ProductsFactory) {
+        if (AuthFactory.isAuthenticated()) {
+            var data = JSON.parse($window.localStorage.getItem('nc-user'));
+            $http.defaults.headers.common.Authorization = 'Token token=' + data.token;
+        } else {
+            $location.path('/login');
+        }
+
+        $rootScope.$on('$routeChangeStart', function() {
+            if(!AuthFactory.isAuthenticated()) {
+                $location.path('/login');
+            } else {
+                ProductsFactory.getProducts();
+            }
+        });
+    });
