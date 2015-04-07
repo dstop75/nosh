617d1da076c220c8bd327efae757257ca5018f4f
Dan Stoppelman <dstop75@gmail.com>
2015-04-07 09:58:22 -0400

    add more angular modules

--- frontend/test/karma.conf.js
+++ frontend/test/karma.conf.js
@@ -23,7 +23,12 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
       'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-aria/angular-aria.js',
+      'bower_components/angular-cookies/angular-cookies.js',
+      'bower_components/angular-messages/angular-messages.js',
+      'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
