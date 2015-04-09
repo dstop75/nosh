1d13260b995552a169303bdb33cd49a31c7aaf17
Dan Stoppelman <dstop75@gmail.com>
2015-04-09 11:02:13 -0400

    whitespace

--- frontend/app/scripts/controllers/ProductsController.js
+++ frontend/app/scripts/controllers/ProductsController.js
@@ -1,18 +1,20 @@
 'use strict';
 
-angular.module('MainController').controller('ProductsController', productsController);
+angular
+    .module('MainController')
+    .controller('ProductsController', productsController);
 
-productsController.$inject = ['ProductsFactory', '$location'];
+    productsController.$inject = ['ProductsFactory', '$location'];
 
-function productsController(ProductsFactory, $location) {
-    var vm = this;
-    vm.products = ProductsFactory.products;
-    vm.newProduct = ProductsFactory.newProduct;
+    function productsController(ProductsFactory, $location) {
+        var vm = this;
+        vm.products = ProductsFactory.products;
+        vm.newProduct = ProductsFactory.newProduct;
 
-    vm.addProduct = function(newProduct) {
-        ProductsFactory.addProduct(newProduct).then(function() {
-            vm.newProduct = {};
-            $location.path('/products');
-        });
-    };
-}
+        vm.addProduct = function(newProduct) {
+            ProductsFactory.addProduct(newProduct).then(function() {
+                vm.newProduct = {};
+                $location.path('/products');
+            });
+        };
+    }
