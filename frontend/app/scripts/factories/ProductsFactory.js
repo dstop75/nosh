de1f918b66218296f3e977eb90dc4eb400bd6008
Dan Stoppelman <dstop75@gmail.com>
2015-04-09 16:23:08 -0400

    handle new product separately with or without image

--- frontend/app/scripts/factories/ProductsFactory.js
+++ frontend/app/scripts/factories/ProductsFactory.js
@@ -7,44 +7,55 @@ angular
 
         var addProduct = function(newProduct) {
             var suffix,
-            file = newProduct.image[0],
             user = JSON.parse($window.localStorage.getItem('nc-user')),
             railsConfig = {
                 headers: {
                     'AUTHORIZATION': 'Token token=' + user.token
                 }
-            },
-            urlReady = file.type.replace(/[%&\/#"\\]/g, function(m) {
-                return (m === '"' || m === '\\') ? ' ' : '%' + m.charCodeAt(0).toString(16);
-            });
+            };
 
-            return $http.get(ServerUrl + '/amazon/sign_key/' + urlReady)
-            .success(function(response) {
-                suffix = response.key;
-                $upload.upload({
-                    url: 'https://s3.amazonaws.com/nosh-cookie-co',
-                    type: 'POST',
-                    fields: {
-                        key: response.key,
-                        AWSAccessKeyId: response.access_key,
-                        policy: response.policy,
-                        signature: response.signature,
-                        'Content-Type': file.type === '' ? 'application/octet-stream' : file.type
-                    },
-                    file: file
+            if (newProduct.image) {
+                var file = newProduct.image[0],
+                urlReady = file.type.replace(/[%&\/#"\\]/g, function(m) {
+                    return (m === '"' || m === '\\') ? ' ' : '%' + m.charCodeAt(0).toString(16);
                 });
-            })
-            .then(function() {
+                return $http.get(ServerUrl + '/amazon/sign_key/' + urlReady)
+                .success(function(response) {
+                    suffix = response.key;
+                    $upload.upload({
+                        url: 'https://s3.amazonaws.com/nosh-cookie-co',
+                        type: 'POST',
+                        fields: {
+                            key: response.key,
+                            AWSAccessKeyId: response.access_key,
+                            policy: response.policy,
+                            signature: response.signature,
+                            'Content-Type': file.type === '' ? 'application/octet-stream' : file.type
+                        },
+                        file: file
+                    });
+                })
+                .then(function() {
+                    var data = {
+                        product: {
+                            name: newProduct.name,
+                            description: newProduct.description,
+                            price: newProduct.price,
+                            image_url: 'https://s3.amazonaws.com/nosh-cookie-co/' + suffix
+                        }
+                    };
+                    $http.post(ServerUrl + '/admin/products', data, railsConfig);
+                });
+            } else {
                 var data = {
                     product: {
                         name: newProduct.name,
                         description: newProduct.description,
-                        price: newProduct.price,
-                        image_url: 'https://s3.amazonaws.com/nosh-cookie-co/' + suffix
+                        price: newProduct.price
                     }
                 };
-                $http.post(ServerUrl + '/admin/products', data, railsConfig);
-            });
+                return $http.post(ServerUrl + '/admin/products', data, railsConfig);
+            }
         };
 
         var getProducts = function() {
