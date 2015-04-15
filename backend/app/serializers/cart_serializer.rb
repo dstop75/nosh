ac694b8b89b8810df1b05ef5d12c8ac260750652
Dan Stoppelman <dstop75@gmail.com>
2015-04-15 10:53:40 -0400

    fix serializers for heroku

--- backend/app/serializers/cart_serializer.rb
+++ backend/app/serializers/cart_serializer.rb
@@ -1,7 +1,7 @@
 class CartSerializer < ActiveModel::Serializer
   attributes :id, :created_at
 
-  has_and_belongs_to_many :products
+  # has_and_belongs_to_many :products
 
   def created_at
     object.created_at.strftime('%m-%d-%Y')
--- backend/app/serializers/order_serializer.rb
+++ backend/app/serializers/order_serializer.rb
@@ -2,7 +2,7 @@ class OrderSerializer < ActiveModel::Serializer
   attributes :id, :cart_id, :created_at
 
   has_one :cart
-  has_and_belongs_to_many :products
+  # has_and_belongs_to_many :products
 
   def created_at
     object.created_at.strftime('%m-%d-%Y')
