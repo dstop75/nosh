e39b35c377ea4d4d96dcf7899487dc222703c9dd
Dan Stoppelman <dstop75@gmail.com>
2015-04-21 19:16:05 -0400

    whitespace

--- backend/spec/routing/users_routing_spec.rb
+++ backend/spec/routing/users_routing_spec.rb
@@ -1,7 +1,7 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe UsersController, type: :routing do
-  describe "routing" do
+  describe 'routing' do
 
     it 'routes to #sign_in' do
       expect(post: 'users/sign_in').to route_to('users#sign_in')
@@ -11,24 +11,24 @@ RSpec.describe UsersController, type: :routing do
       expect(get: 'users/sign_out').to route_to('users#sign_out')
     end
 
-    it "routes to #index" do
+    it 'routes to #index' do
       expect(get: 'admin/users').to route_to('admin/users#index')
     end
 
-    it "routes to #show" do
-      expect(get: "/users/1").to route_to("users#show", id: "1")
+    it 'routes to #show' do
+      expect(get: '/users/1').to route_to('users#show', id: '1')
     end
 
-    it "routes to #create" do
-      expect(post: "/users").to route_to("users#create")
+    it 'routes to #create' do
+      expect(post: '/users').to route_to('users#create')
     end
 
-    it "routes to #update" do
-      expect(put: "/users/1").to route_to("users#update", id: "1")
+    it 'routes to #update' do
+      expect(put: '/users/1').to route_to('users#update', id: '1')
     end
 
-    it "routes to #destroy" do
-      expect(delete: "/users/1").to route_to("users#destroy", id: "1")
+    it 'routes to #destroy' do
+      expect(delete: '/users/1').to route_to('users#destroy', id: '1')
     end
 
   end
