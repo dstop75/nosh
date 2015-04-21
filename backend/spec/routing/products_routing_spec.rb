c4d6164a136d245329381e2533a272b5077da517
Dan Stoppelman <dstop75@gmail.com>
2015-04-21 19:21:01 -0400

    add and pass routing spec: products#show

--- backend/spec/routing/products_routing_spec.rb
+++ backend/spec/routing/products_routing_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe ProductsController, type: :routing do
       expect(get: '/products').to route_to('products#index')
     end
 
+    it 'routes to #show' do
+      expect(get: '/products/1').to route_to('products#show', id: '1')
+    end
+
   end
 end
