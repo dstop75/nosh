e961f980e47e1153846613ea43294e0cbe6e6a8d
Dan Stoppelman <dstop75@gmail.com>
2015-04-21 19:32:46 -0400

    add and pass routing spec: carts#show

--- backend/spec/routing/carts_routing_spec.rb
+++ backend/spec/routing/carts_routing_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe CartsController, type: :routing do
       expect(get: "/carts").to route_to("carts#index")
     end
 
+    it "routes to #show" do
+      expect(get: "/carts/1").to route_to("carts#show", id: "1")
+    end
+
   end
 end
