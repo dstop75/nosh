186a9506e6a15767c07a9558c965c2971853de35
Dan Stoppelman <dstop75@gmail.com>
2015-04-21 20:19:51 -0400

    add validations to product model

--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+
   has_and_belongs_to_many :carts
   has_and_belongs_to_many :orders
 end
