a845971faa3fbcd5a6ae8aa8c43d68a421f085dc
Dan Stoppelman <dstop75@gmail.com>
2015-04-21 20:28:10 -0400

    add and pass unit test: Product.create creates a new product

--- backend/spec/models/product_spec.rb
+++ backend/spec/models/product_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Product, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Product, type: :model do
+  before(:all) do
+    DatabaseCleaner.clean
+    @product = FactoryGirl.build(:product)
+  end
+
+  describe '.create' do
+    it 'creates a new product' do
+      expect(Product.create()).to be_a Product
+    end
+
+  end
 end
